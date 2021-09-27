a5acb81299724ef878c796a7622177d0dd70e324
deolaonly <abogeadeola@gmail.com>
2021-09-27 15:47:00 +0100

    fifth

--- Js4/js4.js
+++ Js4/js4.js
@@ -6,5 +6,22 @@
  const age = 25;
  //Boolean
  const hasKids = true;
+//  Null
+const car =null;
+// Undefined
+let test;
+// symbol
+const sym = Symbol();
 
- console.log(typeof hasKids);
+// REFERENCE TYPES -OBJECTS
+// Array
+const hobbies = ['movies', 'music'];
+
+// object literal
+const address =  {                               
+    city: 'Boston' ,
+    state: 'MA'
+} 
+const today = new Date();
+console.log(today);
+console.log(typeof today );
